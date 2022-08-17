04cb6b10144528e7670bbfbe1e9bfc6223f9a2a5
Mateus <mateusdesouzaitacarambi@gmail.com>
2022-08-17 18:57:54 -0300

    :heavy_check_mark: Desafio concluído

---
+++ desafio-24/scrollReveal.js
@@ -0,0 +1,5 @@
+window.sr =ScrollReveal({ reset: true });
+
+sr.reveal('header h1', { duration: 2500});
+sr.reveal('header .container_botoes', { duration: 4000});
+sr.reveal('aside p, .trailer', { duration: 5000});
