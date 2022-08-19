8342e5415f6fd7b15f9a8d00d64521d788d082cf
Mateus Itacarambi <mateusdesouzaitacarambi@gmail.com>
2022-08-19 08:46:46 -0300

    :heavy_check_mark: Desafio concluído

--- desafio-24/scrollReveal.js
+++ desafio-24/scrollReveal.js
@@ -1,5 +1,5 @@
-window.sr =ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('header h1', { duration: 2500});
-sr.reveal('header .container_botoes', { duration: 4000});
-sr.reveal('aside p, .trailer', { duration: 5000});
+sr.reveal('header h1, header .container_botoes, aside p, .trailer', { duration: 2500 });
+sr.reveal('header h1', {scale: 0.8});
+sr.reveal('header .container_botoes, aside p, .trailer', {delay: 1000});
