e515075df51a3f1358f38952665052fd6989a24b
Mateus Itacarambi <mateusdesouzaitacarambi@gmail.com>
2022-08-19 12:20:35 -0300

    Delete scrollReveal.js

--- desafio-28/scrollReveal.js
+++
@@ -1,4 +0,0 @@
-window.sr =ScrollReveal({ reset: true });
-
-sr.reveal('header h1, header p, header button', { duration: 1500});
-sr.reveal('#ultimo-jogo, #card-bem-vindo, #ultimas-noticias h2, #card-noticia, #ultimas-noticias button', { duration: 1000});
