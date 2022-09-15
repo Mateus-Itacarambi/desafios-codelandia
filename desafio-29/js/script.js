6ccc39194fe33142b54eddc02e2d19d865249e80
Mateus Itacarambi <mateusdesouzaitacarambi@gmail.com>
2022-09-15 10:09:37 -0300

    Adicionando funcionalidade

--- desafio-29/js/script.js
+++ desafio-29/js/script.js
@@ -1,5 +1,5 @@
 const btnMenu = document.getElementById('menu');
-const btnBusca = document.getElementById('busca');
+const btnVerMais = document.getElementById('ver-mais');
 
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault();
@@ -7,5 +7,19 @@ function toggleMenu(event){
     nav.classList.toggle('active');
 };
 
+function toggleVerMais(event){
+    if(event.type === 'touchstart') event.preventDefault();
+    const containerCard = document.getElementById('container-card');
+    containerCard.classList.toggle('active');
+    if (containerCard.className === "container-card active"){
+        btnVerMais.innerText = "Ver menos";
+    } else{
+        btnVerMais.innerText = "Ver mais";
+    };
+};
+
 btnMenu.addEventListener('click', toggleMenu);
 btnMenu.addEventListener('touchstart', toggleMenu);
+
+btnVerMais.addEventListener('click', toggleVerMais);
+btnVerMais.addEventListener('touchstart', toggleVerMais);
