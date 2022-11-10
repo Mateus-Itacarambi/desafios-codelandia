1d9ba1dda91dffdfde699ea1979de7fa5dc4298a
Mateus Itacarambi <mateusdesouzaitacarambi@gmail.com>
2022-11-10 10:08:24 -0300

    Adicionando o desafio 16

---
+++ desafio-16/js/script.js
@@ -0,0 +1,10 @@
+const btnMenu = document.getElementById('menu');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMenu.addEventListener('touchstart', toggleMenu);
+btnMenu.addEventListener('click', toggleMenu);
